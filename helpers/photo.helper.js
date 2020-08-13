bda411ec4e86c2bdf7d7f0139011d53f949b04a8
Minh Do <minhdh@coderschool.vn>
2020-08-14 01:33:54 +0700

    refactor photo resize

--- helpers/photo.helper.js
+++ helpers/photo.helper.js
@@ -8,11 +8,9 @@ photoHelper.resize = async (req, res, next) => {
       req.file.destination =
         req.file.destination.split("\\").join("/").split("server/")[1] + "/";
       req.file.path = req.file.path.split("\\").join("/").split("server/")[1];
-      Jimp.read(req.file.path, async (err, image) => {
-        if (err) next(err);
-        const img = await image.scaleToFit(400, 400).write(req.file.path);
-        next();
-      });
+      const image = await Jimp.read(req.file.path);
+      await image.scaleToFit(400, 400).write(req.file.path);
+      next();
     } catch (err) {
       next(err);
     }
